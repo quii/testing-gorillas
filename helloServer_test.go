9e8deeea3d3346c1964a2cfcbff53e75f9e84897
Chris James <qui666@gmail.com>
2016-02-28 19:55:06 +0000

    better example

--- helloServer_test.go
+++ helloServer_test.go
@@ -1,7 +1,6 @@
 package main
 
 import (
-	"io/ioutil"
 	"net/http"
 	"net/http/httptest"
 	"testing"
@@ -19,19 +18,11 @@ func IgnoreTestMyHandler(t *testing.T) {
 }
 
 func TestMyRouterAndHandler(t *testing.T) {
-	svr := httptest.NewServer(newHelloServer())
-	defer svr.Close()
+	req, _ := http.NewRequest("GET", "/hello/chris", nil)
+	res := httptest.NewRecorder()
+	newHelloServer().ServeHTTP(res, req)
 
-	res, err := http.Get(svr.URL + "/hello/chris")
-
-	if err != nil {
-		t.Fatal("Problem calling hello server", err)
-	}
-
-	greeting, err := ioutil.ReadAll(res.Body)
-	res.Body.Close()
-
-	if string(greeting) != "Hello, chris" {
-		t.Error("Expected hello Chris but got ", greeting)
+	if res.Body.String() != "Hello, chris" {
+		t.Error("Expected hello Chris but got ", res.Body.String())
 	}
 }
